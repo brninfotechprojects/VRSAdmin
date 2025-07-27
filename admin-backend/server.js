cd8465e4a22a10973ae0dec0e0d1d60a79eacb44
Manjunadh <manjunadhb@gmail.com>
2025-07-27 20:36:21 +0530

    imported path library

--- admin-backend/server.js
+++ admin-backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 dotenv.config();
 const app = express();
